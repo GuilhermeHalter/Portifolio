7a12cd5426b0c4e31ac3953390f0d8466fc589ac
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-05-14 22:12:34 -0300

    Editing Dark theme

--- Frontend-HTML/assets/javaScript/theme.js
+++ Frontend-HTML/assets/javaScript/theme.js
@@ -2,16 +2,21 @@ const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 const logoImage = document.getElementById('logoImage');
 const backgroundImage = document.getElementById('backgroundImage');
+const backgroundImages = document.getElementById('backgroundImages');
 
 themeToggle.addEventListener('click', () => {
     if (body.classList.contains('dark-theme')) {
         body.classList.remove('dark-theme');
         logoImage.src = "./assets/media/Ggreen-removebg-preview.png"; 
         backgroundImage.src = "./assets/media/backgroundgreen-removebg-preview.png";
+        backgroundImages.src = "./assets/media/backgroundgreen-removebg-preview.png";
+
     } else {
         body.classList.add('dark-theme');
         logoImage.src = "./assets/media/G-removebg-preview.png"; 
         backgroundImage.src = "./assets/media/background-removebg-preview.png";
+        backgroundImages.src = "./assets/media/background-removebg-preview.png";
+
     }
 });
 
