a2993befd2507f346af0ef1904b5be4690398111
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-09-02 21:28:35 -0300

    Create file projects.json

--- Frontend-React/src/components/ProjectsComp.jsx
+++ Frontend-React/src/components/ProjectsComp.jsx
@@ -6,34 +6,7 @@ import '../styles/Components/ProjectsComp.css';
 import { IoIosExit } from "react-icons/io";
 
 const projects = [
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/portifolio-20d01.appspot.com/o/suiteStore.jpeg?alt=media&token=8996ea9f-6f21-48e2-bf0d-124bf2202914',
-        title: 'Suite Store',
-        description: "Suite Store is the project I created to simplify the management and control of your store's inventory. With it, you can keep everything under control, from inventory to sales.",
-        github: 'https://github.com/GuilhermeHalter/Desafio-ponto-vendas-PDV-',
-        site: 'https://desafio-ponto-vendas-pdv.vercel.app/'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/portifolio-20d01.appspot.com/o/ArduinoSuino.jpeg?alt=media&token=2b726efa-529d-4c03-aa40-626dccfc9125',
-        title: 'Controle de Conforto Térmico em Creche de Suínos',
-        description: 'I developed this project in my technical course in Computer Science, with the aim of controlling thermal comfort in the piglet nursery, using temperature and humidity sensors.',
-        github: 'https://github.com/GuilhermeHalter/Projeto_ControleTermicoSuinos',
-        site: '' // Este projeto não tem site
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/portifolio-20d01.appspot.com/o/Portifolio%2FprintMuseu.jpeg?alt=media&token=17beef85-cb23-4957-b9be-9448a1f78ab5',
-        title: 'Museu do Velho Mundo',
-        description: 'The Old World Museum is a project I created to simulate a virtual museum, where you can view artworks and sculptures by various renowned artists.',
-        github: 'https://github.com/GuilhermeHalter/MuseuVelhoMundo-react',
-        site: 'https://museu-velho-mundo-react.vercel.app/'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/portifolio-20d01.appspot.com/o/Portifolio%2FSolve4u.png?alt=media&token=a977aeb9-0998-45c3-97f3-4d9902522dc6',
-        title: 'Solve4u',
-        description: 'Solve4u is an online platform developed to streamline project management, providing an organized and efficient approach to coordinating tasks and team members.',
-        github: 'https://github.com/GuilhermeHalter/Solve4u',
-        site: 'https://solve4u.vercel.app/'
-    },
+   
 ];
 
 const ProjectsComp = () => {
